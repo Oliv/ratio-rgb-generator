c0441ddffc3f722b2c4c24eed62d25031b446f61
Oliv <jahsoul@gmail.com>
2017-10-18 14:52:50 +0200

    Initial drop

---
+++ README.md
@@ -0,0 +1,20 @@
+# Ratio RGB generator
+
+Returns a rgb color from green to red from a ratio between 0 and 1
+
+
+## Installation
+
+```bash
+$ npm install https://github.com/Oliv/ratio-rgb-generator.git
+```
+
+
+## Usage
+
+```js
+import generateRGB from 'ratio-rgb-generator';
+
+generateRGB(ratio);
+```
+
---
+++ ratio-rgb-generator.js
@@ -0,0 +1,21 @@
+export default function generate(ratio) {
+  let min = 60, max = 220, breakdown = .5, r, g, b = 60;
+
+  let a1 = (() => (max - min) / breakdown)();
+  let a2 = (() => (min - a1) / breakdown)();
+  let b1 = min;
+  let b2 = a1 - (a2 * breakdown);
+
+  let fn1 = x => a1 * x + b1;
+  let fn2 = x => a2 * x + b2;
+
+  if (ratio <= breakdown) {
+    r = Math.round(fn1(ratio));
+    g = max;
+  } else {
+    r = max;
+    g = Math.round(fn2(ratio));
+  }
+
+  return `rgb(${r},${g},${b})`;
+}
